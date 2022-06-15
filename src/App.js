8e213d05383a086510aa447125413a5de8225a5a
Brett <bdmdiving@gmail.com>
2022-06-15 17:29:51 +1000

    shaded rows

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import contacts from './Contacts.js';
+import React from 'react';
+
+
+//conditional rendering
+// function App() {
+//   let contactlist = contacts.map((contact) => {
+//     return (
+//       <li>
+//         <div>Name: {contact.first_name.toUpperCase()} {contact.last_name.toUpperCase()}</div>
+//         <div>Email: <a href={`mailto:${contact.email}`}>{contact.email}</a></div>
+//         <div>Phone: {contact.phone}</div>
+//       </li>
+//     )
+//   })
+
+//   if (contacts.length > 0) {
+//     return (
+//       <div className="App">
+//         <ul>{contactlist}</ul>
+
+//         <div>Total Number of Contacts: {contacts.length}</div>
+//       </div>
+//     );
+//   } else {
+//     return (<div className="App">Sorry, can't sit here :(</div>)
+//   }
+
+// }
+
+//Ternary Operator
+// function App() {
+//   let contactList = contacts.map((contact) => {
+//     return (
+//       <li>
+//         <div>Name: {contact.first_name.toUpperCase()} {contact.last_name.toUpperCase()}</div>
+//         <div>Email: <a href={`mailto:${contact.email}`}>{contact.email}</a></div>
+//         <div>Phone: {contact.phone}</div>
+//       </li>
+//     )
+//   })
+
+//   if (contacts.length > 0) {
+//     return (
+//       <div className="App">
+//         {contacts.length
+//           ? <div><ul>{contactList}</ul><div>Total Number of Contacts: {contacts.length}</div></div>
+//           : <div>Sorry, no contacts found :(</div>
+//         }
+//       </div>
+//     )
+//   } 
+
+// };
+
+//using react fragment
+// function App() {
+//   let contactList = contacts.map((contact) => {
+//     return (
+//       <li>
+//         <div>Name: {contact.first_name.toUpperCase()} {contact.last_name.toUpperCase()}</div>
+//         <div>Email: <a href={`mailto:${contact.email}`}>{contact.email}</a></div>
+//         <div>Phone: {contact.phone}</div>
+//       </li>
+//     )
+//   })
+
+//   if (contacts.length > 0) {
+//     return (
+//       <div className="App">
+//         { contacts.length
+//           ? <React.Fragment><ul>{contactList}</ul><div>Total Number of Contacts: {contacts.length}</div></React.Fragment>
+//           : <div>Sorry, no contacts found :(</div>
+//           }
+//       </div>
+//     )
+    
+//   } 
+
+// };
+
+//for styling in JSX
 
 function App() {
+  let contactList = contacts.map( (contact) => {
+    return (
+      <tr className="contact">
+        <td className="name">{contact.first_name.toUpperCase()} {contact.last_name.toUpperCase()}</td>
+        <td className="email"><a href={`mailto:${contact.email}`}>{contact.email}</a></td>
+        <td className="phone">{contact.phone}</td>
+      </tr>
+  )})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="contacts">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactList}
+        </tbody>
+      </table>
+      { contacts.length
+        ? <div className="contactSummary">Total Number of Contacts: {contacts.length}</div>
+        : <div>Sorry, no contacts found :(</div>
+      }
     </div>
-  );
+  )
 }
+
 
 export default App;
